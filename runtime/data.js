512a5fa48178b3cb7d10016851d11d7a86e93c64
github-action-benchmark <github@users.noreply.github.com>
2024-06-10 15:29:54 +0000

    add Transform Runtime Metrics (customSmallerIsBetter) benchmark result for c0f4057b0ad4cce992f658fc907d5bac02225c3a

---
+++ runtime/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718033394060,
+  "repoUrl": "https://github.com/oxc-project/monitor-oxc",
+  "entries": {
+    "Transform Runtime Metrics": [
+      {
+        "commit": {
+          "author": {
+            "email": "boshenc@gmail.com",
+            "name": "Boshen",
+            "username": "Boshen"
+          },
+          "committer": {
+            "email": "boshenc@gmail.com",
+            "name": "Boshen",
+            "username": "Boshen"
+          },
+          "distinct": true,
+          "id": "c0f4057b0ad4cce992f658fc907d5bac02225c3a",
+          "message": "w",
+          "timestamp": "2024-06-10T23:28:18+08:00",
+          "tree_id": "fe548651101652dafe606d149ec1b9684d47b420",
+          "url": "https://github.com/oxc-project/monitor-oxc/commit/c0f4057b0ad4cce992f658fc907d5bac02225c3a"
+        },
+        "date": 1718033393188,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Duration",
+            "value": 0.04503611951354839,
+            "unit": "ms"
+          },
+          {
+            "name": "Maximum Resident Set Size",
+            "value": 26732.2,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Average resident set size",
+            "value": 0,
+            "unit": "Kbytes"
+          },
+          {
+            "name": "Average total (data+stack+text) memory use",
+            "value": 0,
+            "unit": "Kbytes"
+          }
+        ]
+      }
+    ]
+  }
+}
